d296bb353cfd7f587244e6382a434689de7f7a40
Apple <apple@192.168.1.10>
2026-09-05 15:56:35 +0500

    style: adjust navbar logo to a refined balanced scale

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export function Navbar() {
             <img
               src="/images/1212.png"
               alt="BFF Logo"
-              className="h-16 w-auto lg:h-20 scale-[1.45] lg:scale-[1.75] origin-left transition-transform duration-300 pointer-events-auto"
+              className="h-14 w-auto lg:h-16 scale-[1.25] lg:scale-[1.4] origin-left transition-transform duration-300 pointer-events-auto"
             />
           </a>
         </div>
