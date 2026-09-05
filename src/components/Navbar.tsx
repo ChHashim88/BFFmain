cda73df5924dbaeb88bfab98407feaa916ef7703
Apple <apple@192.168.1.10>
2026-09-05 16:01:24 +0500

    style: increase navbar logo scale to 1.35x/1.5x while keeping fixed navbar structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export function Navbar() {
             <img
               src="/images/1212.png"
               alt="BFF Logo"
-              className="h-14 w-auto lg:h-16 scale-[1.18] lg:scale-[1.28] origin-left transition-transform duration-300 pointer-events-auto"
+              className="h-14 w-auto lg:h-16 scale-[1.35] lg:scale-[1.5] origin-left transition-transform duration-300 pointer-events-auto"
             />
           </a>
         </div>
